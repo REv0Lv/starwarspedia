927848c378ab724156d1db5f1f65fc4625b43e3d
REv0Lv <colinhugopro@gmail.com>
2023-02-17 16:45:10 +0100

    Create accueil.php

---
+++ frontoffice/accueil.php
@@ -0,0 +1,81 @@
+<?php //Les planètes ?>
+
+<h1 style="text-align: center">Les planètes</h1>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="./img/tatooine.webp" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Tatooine</h5>
+        <a href="./detailsPlanetes/detailsTatooine.php">Plus d'informations</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./img/alderaan.webp" class="card-img-top" alt="alderaan">
+      <div class="card-body">
+        <h5 class="card-title">Alderaan</h5>
+        <a href="./frontoffice/planetes.php">Plus d'informations</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./img/yavinIV.webp" class="card-img-top" alt="yavinIV">
+      <div class="card-body">
+        <h5 class="card-title">Yavin IV</h5>
+        <a href="./frontoffice/planetes.php">Plus d'informations</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./img/hoth.webp" class="card-img-top" alt="hoth">
+      <div class="card-body">
+        <h5 class="card-title">Hoth</h5>
+        <a href="./frontoffice/planetes.php">Plus d'informations</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<h1 style="text-align: center">Les films</h1>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="./img/starWarsI.webp" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Star Wars I</h5>
+        <a href="./frontoffice/films.php">Plus d'informations</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./img/starWarsII.jpg" class="card-img-top" alt="alderaan">
+      <div class="card-body">
+        <h5 class="card-title">Star Wars II</h5>
+        <a href="./frontoffice/films.php">Plus d'informations</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./img/starWarsIII.jpg" class="card-img-top" alt="yavinIV">
+      <div class="card-body">
+        <h5 class="card-title">Star Wars III</h5>
+        <a href="./frontoffice/films.php">Plus d'informations</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="./img/starWarsIV.webp" class="card-img-top" alt="hoth">
+      <div class="card-body">
+        <h5 class="card-title">Star Wars IV</h5>
+        <a href="./frontoffice/films.php">Plus d'informations</a>
+      </div>
+    </div>
+  </div>
+</div>
