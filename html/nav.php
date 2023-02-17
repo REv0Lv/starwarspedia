8426fc4ae0f1054cbb34daf838d48e0fad933a05
REv0Lv <colinhugopro@gmail.com>
2023-02-17 16:45:20 +0100

    Create nav.php

---
+++ html/nav.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Navigation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <img src="img/starwarspedialogo.jpg" alt="Logo de Star Wars Pedia" style="width:18% ; height:18% ; padding:5px ; border:5px">
+          <div class="collapse navbar-collapse" justify-content-between id="navbarNav" style="position: relative ; padding:20px ; align-content: space-around">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="./frontoffice/planetes.php">Planètes</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./frontoffice/especes.php">Espèces</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./frontoffice/films.php">Films</a>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</html>
